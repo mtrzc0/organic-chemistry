98e71e3cdc8c5213ed612e95b5dc4f0c98779477
Mateusz Trzeciak <trzeciak.3mateusz@gmail.com>
2021-06-12 19:52:27 +0200

    Add files via upload

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,45 +1,58 @@
-const mobileMenuBurgerIcon = document.querySelector('.menu-btn .burger')
-const headerMobileList = document.querySelector('header .mobile-list')
-const headerMobileListLink = document.querySelectorAll('header .mobile-list li')
-let screenWidth = 0
-
+// footer year
 let date = new Date()
 const dateElement = document.querySelector('#date')
 dateElement.innerText = date.getFullYear()
 
-function showMobileMenu() {
-    screenWidth = screen.width
-    if (screenWidth < 650){
-        if (headerMobileList.style.top == '-300px') {
-            headerMobileList.style.top = '60px'
-            headerMobileListLink.forEach(link => {
-                link.style.transform = 'scale(1)'
-            })
-            mobileMenuBurgerIcon.classList.toggle('changeBars')
-        }
-        else {
-            headerMobileList.style.top = '-300px'
-            headerMobileListLink.forEach(link => {
-                link.style.transform = 'scale(0)'
-            })
-            mobileMenuBurgerIcon.classList.toggle('changeBars')
-        }
-    } else if (screenWidth > 650 && screenWidth < 1150) {
-        if (headerMobileList.style.top == '-300px') {
-            headerMobileList.style.top = '80px'
-            headerMobileListLink.forEach(link => {
-                link.style.transform = 'scale(1)'
-            })
-            mobileMenuBurgerIcon.classList.toggle('changeBars')
-        }
-        else {
-            headerMobileList.style.top = '-300px'
-            headerMobileListLink.forEach(link => {
-                link.style.transform = 'scale(0)'
-            })
-            mobileMenuBurgerIcon.classList.toggle('changeBars')
-        }
+// hamburger animation
+const mobileMenu = document.querySelector('header .mobile-list');
+const mobileMenuLink = document.querySelectorAll('.mobile-list ul li a');
+
+const toggle = document.querySelector('.menu-btn .burger')
+const menuTL = gsap.timeline({ defaults: { duration: .8 } });
+let isOpen = false
+
+const screenObject = {
+    smartphoneWidth: 650,
+    tabletWidth: 1150
+}
+
+function smartphoneWidth () {
+    if (isOpen === false) {
+        menuTL
+            .to(mobileMenu, { translateX: '0vw'  })
+            .fromTo(mobileMenuLink, { scale: 0 }, { scale: 1 }, '-=0.8')
+        toggle.classList.add('changeBars')
+        isOpen = true
+    } else {
+        menuTL
+            .to(mobileMenu, { translateX: '-80vw' })
+            .fromTo(mobileMenuLink, { scale: 1 }, { scale: 0 }, '-=0.8')
+        toggle.classList.remove('changeBars')
+        isOpen = false
     }
 }
 
-mobileMenuBurgerIcon.addEventListener('click', showMobileMenu)
+function tabletWidth() {
+    if (isOpen === false) {
+        menuTL
+            .to(mobileMenu, { translateX: '0vw' })
+            .fromTo(mobileMenuLink, { scale: 0 }, { scale: 1 }, '-=0.8')
+        toggle.classList.add('changeBars')
+        isOpen = true
+    } else {
+        menuTL
+            .to(mobileMenu, { translateX: '-80vw' })
+            .fromTo(mobileMenuLink, { scale: 1 }, { scale: 0 }, '-=0.8')
+        toggle.classList.remove('changeBars')
+        isOpen = false
+    }
+}
+
+toggle.addEventListener('click', () => {
+    if (screen.width < screenObject.smartphoneWidth) {
+        smartphoneWidth()
+    } else if (screen.width > screenObject.smartphoneWidth && screen.width < screenObject.tabletWidth) {
+        tabletWidth()
+    }
+
+})
